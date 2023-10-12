af1264ba04acaab5baac29f21ccba4c55f7d8072
Mohsen Dorparasti <dorparasti@gmail.com>
2023-10-12 16:42:31 +0330

    [Fix] : fix function being called directly inside useState in useLoading

--- states/loading.state.js
+++ states/loading.state.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { getLoadingInfo } from "../utils";
 
 export function useLoading(loading) {
-  const [loadingInfo, setLoadingInfo] = useState(getLoadingInfo(loading));
+  const [loadingInfo, setLoadingInfo] = useState(() => getLoadingInfo(loading));
 
   function setIsLoading(isLoading) {
     setLoadingInfo((prev) => ({
